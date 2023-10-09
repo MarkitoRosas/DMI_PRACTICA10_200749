56d1408b63f313e2501b27ec25b354a8a8b50822
MarkitoRosas <marcoantonio.rosasgonzalez@utxicotepec.edu.mx>
2023-10-09 09:43:26 -0600

    Practica 10

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # DMI_PRACTICA10_200749
+
+[![Encabezado.png](https://i.postimg.cc/PJKtvHNC/Encabezado.png)](https://postimg.cc/K3kXCdPb)
+
+<div align="center">
+  
+# Universidad Tecnológica de Xicotepec de Juárez
+
+## Ingeniería en Desarrollo y Gestión de Software
+
+## Desarrollo Movil Integral
+
+## Marco Antonio Rosas Gonzalez
+ 
+## 10° "A"
+
+## Septiempre - Diciembre
+
+## Descripción: Este proyecto consiste en una muestra de una aplicación desarrollada en Flutter que presenta la página de inicio de la aplicación "MovieApp-200749". En esta página de inicio, se encuentran los siguientes componentes clave:
+
+**Home.dart**:
+Dentro de este archivo, se encuentra la clase Home, que representa la página principal de la aplicación. En el método initState, se llama a la función _loadJson para cargar datos de películas utilizando HttpHandler. Además, se define un estilo de fuente personalizado y se crea la estructura principal de la aplicación, que incluye una AppBar (Barra de Navegación Superior), un Drawer (Menú Lateral) y un BottomNavigationBar (Barra de Navegación Inferior).
+
+1. **AppBar (Barra de Navegación Superior)**:
+   La AppBar muestra el título "MovieApp-200749" en el centro y un icono de búsqueda en la esquina derecha. Aquí es donde se puede realizar la navegación y las búsquedas.
+
+2. **Menú Lateral (Drawer)**:
+   El menú lateral se despliega deslizando desde el borde izquierdo de la pantalla o al tocar el icono de menú en la AppBar. Contiene las siguientes opciones:
+   - "Peliculas": Esta opción muestra películas y utiliza un estilo de fuente personalizado para dar formato al texto.
+   - "Television": Muestra programas de televisión y utiliza el mismo estilo de fuente personalizado.
+   - "Cerrar": Permite cerrar el menú lateral y volver a la página de inicio.
+
+3. **Barra de Navegación Inferior (BottomNavigationBar)**:
+   La barra de navegación inferior contiene tres elementos:
+   - "Populares": Muestra películas populares y presenta un ícono de pulgar hacia arriba.
+   - "Proximamente": Muestra películas próximas a estrenarse y tiene un ícono de actualización.
+   - "Mejor valorados": Muestra películas mejor valoradas y cuenta con un ícono de estrella.
+
+**En resumen, este proyecto se enfoca en la creación de una interfaz de usuario para la aplicación "MovieApp-200749". La página de inicio incluye una AppBar, un menú lateral y una barra de navegación inferior, y se utiliza Flutter para mostrar información detallada sobre películas y programas de televisión.**
+
+
+<p align="center">
+<img src="./assets/1.png" width="200" alt="Captura de Pantalla 1">
+<img src="./assets/2.png" width="200" alt="Captura de Pantalla 2">
+<img src="./assets/3.png" width="200" alt="Captura de Pantalla 3">
+</p>
+
+
+
+
+&nbsp;
+&nbsp;
+
+&nbsp;
+&nbsp;
+
+<br>
+<br>
---
+++ lib/media_list_item.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp_200749/model/Media.dart';
+
+
+
+class MediaListItem extends StatelessWidget {
+  final Media media;
+
+  MediaListItem(this.media);
+
+  @override
+  Widget build(BuildContext context) {
+    return  new Card(
+      child:new Column(
+        children: <Widget>[
+          new Container(
+            child: new Stack(
+              children: <Widget>[
+            new FadeInImage.assetNetwork(
+            placeholder: "assets/si.gif",
+            image: media.getBackDropUrl(),
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: 200.0,
+            fadeInDuration: new Duration(milliseconds: 500),
+            ),
+            new Positioned(
+              left: 0.0,
+              bottom: 0.0,
+              right: 0.0,
+              child: new Container(
+                decoration: new BoxDecoration(
+                  color: Colors.grey.withOpacity(0.8),
+                ),
+                constraints: new BoxConstraints.expand(
+                  height: 55.0,
+                ),
+              ),
+            ),
+            new Positioned(
+              left: 10.0,
+              bottom: 10.0,
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Container(
+                    width: 250,
+                    child: new Text(
+                      media.title,
+                      style: new TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      softWrap: false,
+                    ),
+                  ),
+                  new Container(
+                    width: 250.0,
+                    padding: const EdgeInsets.only(top: 3.0),
+                    child: new Text(
+                      media.getGenres(),
+                      style: new TextStyle(color: Colors.white),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            new Positioned(
+              right: 5.0,
+              bottom: 10.0,
+              child: new Column(
+              children: <Widget>[
+                new Row(
+                  children: <Widget>[
+                    new Text(media.voteAverage.toString()),
+                    new Container(width: 4.0,),
+                    new Icon(Icons.star, color: Colors.yellow, size: 16.0,)
+                  ],
+                ),
+                new Container(height: 4.0,),
+                new Row(
+                  children: <Widget>[
+                    new Text(media.getReleaseYear().toString()),
+                    new Container(width: 4.0,),
+                    new Icon(Icons.date_range, color: Colors.white, size: 16.0,)
+                  ],
+                )
+              ],
+            ))
+            ],
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
